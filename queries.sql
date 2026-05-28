9ca2dbbce7377069851c446cccca6eb048c37d82
Devdeep Sarkar <devdeepsarkar1234@gmail.com>
2026-05-29 02:03:53 +0530

    Update queries.sql, repeat_vs_one_time_customers screenshot and spreadsheet

--- queries.sql
+++ queries.sql
@@ -110,9 +110,13 @@ SELECT
     END AS customer_type,
     COUNT(*) AS total_customers
 FROM (
-    SELECT customer_id, COUNT(order_id) AS order_count
-    FROM orders
-    GROUP BY customer_id
+    SELECT 
+        c.customer_unique_id,
+        COUNT(o.order_id) AS order_count
+    FROM orders o
+    JOIN customers c 
+        ON o.customer_id = c.customer_id
+    GROUP BY c.customer_unique_id
 ) sub
 GROUP BY customer_type;
 
